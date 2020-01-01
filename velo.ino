937cc9509947ee694a4b30dad1a8cd5dec306880
DieFaust <diefaust1993@gmail.com>
2020-01-02 00:49:51 +0200

    Refactor wheel counter

--- velo.ino
+++ velo.ino
@@ -4,22 +4,14 @@
 #define WHEEL_PIN 2
 #define PEDAL_PIN 3
 #define PEDAL_LED_PIN 4
-
-# define HOUR_CONSTANT 0.00027 // 1 sec in hours
-# define CALC_TIME 1000 // 1 sec
-# define PEDAL_MULT 12
 # define WHEEL_LENGTH 0.00207 // in km for 26"
-
-# define COM_SET_WHEEL_LENGTH 1# define COM_SET_CALC_TIME 2
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-uint8_t pin;
-boolean pin_enabled;
+uint8_t wheel_pin;
+boolean wheel_pin_enabled;
 
-uint8_t wheel_rate;
-unsigned long timer; // ms
-float wheel_length;
+uint8_t wheel_counter;
 float wheel_speed; // km/h
 float max_wheel_speed; // km/h
 
@@ -28,11 +20,12 @@ float distance; // km
 uint8_t pedal_pin;
 boolean pedal_pin_high;
 
+unsigned long pedal_interval; // ms
+uint8_t pedal_interval_counter;
+
 unsigned long pedal_timer; // ms
 unsigned long tick_timer; // ms
 unsigned long timer_now; // ms
-unsigned long pedal_interval; // ms
-uint8_t pedal_interval_counter;
 
 String bt_cmd; // bluetooth command
 uint8_t bt_cmd_val = -1;
@@ -46,6 +39,17 @@ void setup() {
 void loop() {
     timer_now = millis();
 
+    /*-----------------------------------------------------------------------------*/
+    wheel_pin = digitalRead(WHEEL_PIN);
+    if (wheel_pin == HIGH && !wheel_pin_enabled) {
+        wheel_pin_enabled = true;
+        wheel_counter++;
+        distance += WHEEL_LENGTH;
+    } else if (wheel_pin == LOW) {
+        wheel_pin_enabled = false;
+    }
+
+    /*-----------------------------------------------------------------------------*/
     pedal_pin = digitalRead(PEDAL_PIN);
     if (pedal_pin == HIGH && !pedal_pin_high) {
         pedal_pin_high = true;
@@ -58,6 +62,7 @@ void loop() {
         pedal_pin_high = false;
     }
 
+    /*-----------------------------------------------------------------------------*/
     if (timer_now - tick_timer >= 3000) {
         tick_timer = timer_now;
         int rpm = 0;
@@ -80,6 +85,26 @@ void loop() {
         lcd.print(" rpm");
 
         digitalWrite(PEDAL_LED_PIN, (rpm >= 70 && rpm <= 100) ? HIGH : LOW);
+
+        // display distance
+        lcd.setCursor(0, 1);
+        lcd.print(distance);
+        lcd.print(" km");
+
+        // calculate wheel speed km/h
+        float d = wheel_counter * WHEEL_LENGTH;
+        wheel_counter = 0;
+        wheel_speed = d / 0.00083; // 3 secs in hour
+        if (wheel_speed >= max_wheel_speed) {
+            max_wheel_speed = wheel_speed;
+        }
+        
+        // display wheel speed
+        lcd.setCursor(0, 0);
+        lcd.print("       ");
+        lcd.setCursor(0, 0);
+        lcd.print((int) wheel_speed);
+        lcd.print(" km/h");
     }
 
     delay(1);
@@ -108,18 +133,19 @@ void loop() {
     }
     bt_cmd_val = -1;
 
-    pin = digitalRead(WHEEL_PIN);
-    if (pin == HIGH && !pin_enabled) {
-        pin_enabled = true;
+    wheel_pin = digitalRead(WHEEL_PIN);
+    if (wheel_pin == HIGH && !wheel_pin_enabled) {
+        wheel_pin_enabled = true;
         wheel_rate++;
-    } else if (pin == LOW) {
-        pin_enabled = false;
+        distance += WHEEL_LENGTH;
+    } else if (wheel_pin == LOW) {
+        wheel_pin_enabled = false;
     }
 
     if (millis() - timer >= CALC_TIME) {
         timer = millis();
-        float d = wheel_rate * WHEEL_LENGTH;
-        distance += d;
+        //float d = wheel_rate * WHEEL_LENGTH;
+        //distance += d;
 
         // display distance
         lcd.setCursor(0, 1);
