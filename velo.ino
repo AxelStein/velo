758c5e73df5557e2807b05af74bb577dc27c27e1
DieFaust <diefaust1993@gmail.com>
2020-01-01 23:45:56 +0200

    Refactor pedal counter

--- velo.ino
+++ velo.ino
@@ -2,12 +2,13 @@
 #include <Wire.h>
 
 #define WHEEL_PIN 2
-#define CADENCE_PIN 3
-#define CADENCE_LED_PIN 4
+#define PEDAL_PIN 3
+#define PEDAL_LED_PIN 4
 
 # define HOUR_CONSTANT 0.00027 // 1 sec in hours
 # define CALC_TIME 1000 // 1 sec
-# define CADENCE_MULT 12# define WHEEL_LENGTH 0.00207 // in km for 26"
+# define PEDAL_MULT 12
+# define WHEEL_LENGTH 0.00207 // in km for 26"
 
 # define COM_SET_WHEEL_LENGTH 1# define COM_SET_CALC_TIME 2
 
@@ -24,25 +25,66 @@ float max_wheel_speed; // km/h
 
 float distance; // km
 
-uint8_t cadence_pin;
-boolean cadence_pin_enabled;
+uint8_t pedal_pin;
+boolean pedal_pin_high;
 
-uint8_t cadence_rate; // rpm 70-100
-unsigned long cadence_timer; // ms
+unsigned long pedal_timer; // ms
+unsigned long tick_timer; // ms
+unsigned long timer_now; // ms
+unsigned long pedal_interval; // ms
+uint8_t pedal_interval_counter;
 
 String bt_cmd; // bluetooth command
 uint8_t bt_cmd_val = -1;
 
 void setup() {
-    // put your setup code here, to run once:
     lcd.init();
     lcd.backlight();
     lcd.noBlink();
-    lcd.print("loading");
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    timer_now = millis();
+
+    pedal_pin = digitalRead(PEDAL_PIN);
+    if (pedal_pin == HIGH && !pedal_pin_high) {
+        pedal_pin_high = true;
+        if (pedal_timer != 0) {
+            pedal_interval += (timer_now - pedal_timer);
+            pedal_interval_counter++;
+        }
+        pedal_timer = timer_now;
+    } else if (pedal_pin == LOW) {
+        pedal_pin_high = false;
+    }
+
+    if (timer_now - tick_timer >= 3000) {
+        tick_timer = timer_now;
+        int rpm = 0;
+        if (pedal_interval_counter > 1) {
+            float avg = pedal_interval / pedal_interval_counter; // average interval
+            avg = 1000 / avg;
+            rpm = avg * 60;
+
+            pedal_interval = 0;
+            pedal_interval_counter = 0;
+        } else {
+            pedal_timer = 0;
+        }
+
+        // display rpm
+        lcd.setCursor(9, 0);
+        lcd.print("        ");
+        lcd.setCursor(9, 0);
+        lcd.print(rpm);
+        lcd.print(" rpm");
+
+        digitalWrite(PEDAL_LED_PIN, (rpm >= 70 && rpm <= 100) ? HIGH : LOW);
+    }
+
+    delay(1);
+    
+    /*
     while (Serial.available()) { // пока приходят данные
         char c = Serial.read(); // считываем их
         if (c == '#') {
@@ -103,36 +145,31 @@ void loop() {
             //lcd.print(" km/h");
         }
     }
+    */
 
-    cadence_pin = digitalRead(CADENCE_PIN);
-    if (cadence_pin == HIGH && !cadence_pin_enabled) {
-        cadence_pin_enabled = true;
-        cadence_rate++;
-    } else if (cadence_pin == LOW) {
-        cadence_pin_enabled = false;
-    }
+    
 
-    if (millis() - cadence_timer >= CALC_TIME) {
-        cadence_timer = millis();
+    /*
+    if (millis() - pedal_timer >= CALC_TIME) {
+        pedal_timer = millis();
 
-        cadence_rate *= CADENCE_MULT;
+        pedal_rate *= PEDAL_MULT;
         // display cadence per minute
         lcd.setCursor(9, 0);
         lcd.print("        ");
         lcd.setCursor(9, 0);
-        lcd.print(cadence_rate);
+        lcd.print(pedal_rate);
         lcd.print(" rpm");
 
-        if (cadence_rate >= 70) {
+        if (pedal_rate >= 70) {
             // enable diod
-            digitalWrite(CADENCE_LED_PIN, HIGH);
+            digitalWrite(PEDAL_LED_PIN, HIGH);
         } else {
             // disable diod
-            digitalWrite(CADENCE_LED_PIN, LOW);
+            digitalWrite(PEDAL_LED_PIN, LOW);
         }
 
-        cadence_rate = 0;
+        pedal_rate = 0;
     }
-
-    delay(1);
+    */
 }
