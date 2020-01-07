8dbca9998e6229459c2214b492577de7a49ed0ac
DieFaust <diefaust1993@gmail.com>
2020-01-07 14:52:59 +0200

    Refactor algorithm

--- velo.ino
+++ velo.ino
@@ -10,24 +10,23 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 SoftwareSerial BTSerial(2, 3); // RX | TX
 
-float wheel_length;
+float wheel_length; // km
 uint8_t wheel_pin;
-boolean wheel_pin_enabled;
-
+boolean wheel_pin_high;
 uint8_t wheel_counter;
 float wheel_speed; // km/h
 float max_wheel_speed; // km/h
-
+unsigned long wheel_timer; // ms
+unsigned long wheel_interval; // ms
+uint8_t wheel_interval_counter;
 float distance; // km
 
 uint8_t pedal_pin;
 boolean pedal_pin_high;
-
 unsigned long pedal_interval; // ms
 uint8_t pedal_interval_counter;
-
 unsigned long pedal_timer; // ms
-unsigned long tick_timer; // ms
+
 unsigned long timer_now; // ms
 
 String bt_cmd; // bluetooth command
@@ -41,23 +40,113 @@ void setup() {
     lcd.noBlink();
 
     calc_wheel_length(EEPROM.read(0));
+    
+    display_wheel_speed(0);
+    display_rpm(0);
+    display_distance();
 }
 
 void calc_wheel_length(int wheel_diameter) {
-  if (wheel_diameter < 0) {
-      BTSerial.println("Error: wheel_diameter can't be < 0");
-      return;
-  }
-  if (wheel_diameter == 255) {
-      wheel_diameter = 65; // default
-      EEPROM.write(0, wheel_diameter);
+    if (wheel_diameter < 0) {
+        BTSerial.println("Error: wheel_diameter can't be < 0");
+        return;
+    }
+    if (wheel_diameter == 255) {
+        wheel_diameter = 65; // default
+        EEPROM.write(0, wheel_diameter);
     }
     wheel_diameter *= 3.14;
     wheel_length = wheel_diameter / 100000.000000;
 }
 
+void display_rpm(int rpm) {
+    lcd.setCursor(9, 0);
+    lcd.print("        ");
+    lcd.setCursor(9, 0);
+    lcd.print(rpm);
+    lcd.print(" rpm");
+
+    digitalWrite(PEDAL_LED_PIN, (rpm >= 70 && rpm <= 100) ? HIGH : LOW);
+}
+
+void display_wheel_speed(float s) {
+    lcd.setCursor(0, 0);
+    lcd.print("       ");
+    lcd.setCursor(0, 0);
+    lcd.print((int) s);
+    lcd.print(" km/h");
+}
+
+void display_distance() {
+    lcd.setCursor(0, 1);
+    lcd.print(distance);
+    lcd.print(" km");
+}
+
 void loop() {
+    timer_now = millis();
+
+    wheel_pin = digitalRead(WHEEL_PIN);
+    if (wheel_pin == HIGH && !wheel_pin_high) {
+        wheel_pin_high = true;
+        if (wheel_timer != 0) {
+            wheel_interval += (timer_now - wheel_timer);
+            wheel_interval_counter++;
+            distance += wheel_length;
+        }
+        wheel_timer = timer_now;
+    } else if (wheel_pin == LOW) {
+        wheel_pin_high = false;
+    }
+
+    if (wheel_interval >= 3000) {
+        float avg = wheel_interval / wheel_interval_counter; // average interval
+        avg = 1000 / avg;
+        wheel_speed = (avg * 60) * 60 * wheel_length;
+        if (wheel_speed >= max_wheel_speed) {
+            max_wheel_speed = wheel_speed;
+        }
+        display_wheel_speed(wheel_speed);
+        display_distance();
+        
+        wheel_interval = 0;
+        wheel_interval_counter = 0;
+    } else if (wheel_timer != 0 && (timer_now - wheel_timer) >= 3000) { // idle
+        wheel_timer = 0;
+        display_wheel_speed(0);
+        display_distance();
+    }
+
     /*-----------------------------------------------------------------------------*/
+    pedal_pin = digitalRead(PEDAL_PIN);
+    if (pedal_pin == HIGH && !pedal_pin_high) {
+        pedal_pin_high = true;
+        if (pedal_timer != 0) {
+            pedal_interval += (timer_now - pedal_timer);
+            pedal_interval_counter++;
+        }
+        pedal_timer = timer_now;
+    } else if (pedal_pin == LOW) {
+        pedal_pin_high = false;
+    }
+
+    if (pedal_interval >= 3000) {
+        float avg = pedal_interval / pedal_interval_counter; // average interval
+        avg = 1000 / avg;
+        int rpm = avg * 60;
+        display_rpm(rpm);
+        
+        pedal_interval = 0;
+        pedal_interval_counter = 0;
+    } else if (pedal_timer != 0 && (timer_now - pedal_timer) >= 3000) { // idle
+        pedal_timer = 0;
+        display_rpm(0);
+    }
+
+    delay(10);
+    
+
+/*-----------------------------------------------------------------------------*/
     /*
     while (BTSerial.available()) { // пока приходят данные
         char c = BTSerial.read(); // считываем их
@@ -86,77 +175,6 @@ void loop() {
     */
 
     /*-----------------------------------------------------------------------------*/
-    
-    timer_now = millis();
-
-    /*-----------------------------------------------------------------------------*/
-    wheel_pin = digitalRead(WHEEL_PIN);
-    if (wheel_pin == HIGH && !wheel_pin_enabled) {
-        wheel_pin_enabled = true;
-        wheel_counter++;
-        distance += wheel_length;
-    } else if (wheel_pin == LOW) {
-        wheel_pin_enabled = false;
-    }
-
-    /*-----------------------------------------------------------------------------*/
-    pedal_pin = digitalRead(PEDAL_PIN);
-    if (pedal_pin == HIGH && !pedal_pin_high) {
-        pedal_pin_high = true;
-        if (pedal_timer != 0) {
-            pedal_interval += (timer_now - pedal_timer);
-            pedal_interval_counter++;
-        }
-        pedal_timer = timer_now;
-    } else if (pedal_pin == LOW) {
-        pedal_pin_high = false;
-    }
-
-    /*-----------------------------------------------------------------------------*/
-    if (timer_now - tick_timer >= 3000) {
-        tick_timer = timer_now;
-        int rpm = 0;
-        if (pedal_interval_counter > 1) {
-            float avg = pedal_interval / pedal_interval_counter; // average interval
-            avg = 1000 / avg;
-            rpm = avg * 60;
-
-            pedal_interval = 0;
-            pedal_interval_counter = 0;
-        } else {
-            pedal_timer = 0;
-        }
-
-        // display rpm
-        lcd.setCursor(9, 0);
-        lcd.print("        ");
-        lcd.setCursor(9, 0);
-        lcd.print(rpm);
-        lcd.print(" rpm");
-
-        digitalWrite(PEDAL_LED_PIN, (rpm >= 70 && rpm <= 100) ? HIGH : LOW);
-
-        // display distance
-        lcd.setCursor(0, 1);
-        lcd.print(distance);
-        lcd.print(" km");
-
-        // calculate wheel speed km/h
-        float d = wheel_counter * wheel_length;
-        wheel_counter = 0;
-        wheel_speed = d / 0.00083; // 3 secs in hour
-        if (wheel_speed >= max_wheel_speed) {
-            max_wheel_speed = wheel_speed;
-        }
-        
-        // display wheel speed
-        lcd.setCursor(0, 0);
-        lcd.print("       ");
-        lcd.setCursor(0, 0);
-        lcd.print((int) wheel_speed);
-        lcd.print(" km/h");
-    }
-
     /*
         if (BTSerial.available()) {
           BTSerial.println("$" + wheel_speed);
@@ -168,5 +186,5 @@ void loop() {
     
     /*-----------------------------------------------------------------------------*/
 
-    delay(1);
+    
 }
