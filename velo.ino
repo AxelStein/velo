2b0ddfc769ba7649ceabdc0b112b953010d9415d
DieFaust <diefaust1993@gmail.com>
2020-07-07 03:05:11 +0300

    Add led

--- velo.ino
+++ velo.ino
@@ -10,6 +10,8 @@
 #define WHEEL_PIN 2
 #define PEDAL_PIN 3
 #define BTN_PIN 4
+#define TEMP_PIN A1
+#define LED_PIN 5
 #define LONG_PRESS_TIME 750
 #define EEPROM_WHEEL_DIAMETER 0
 #define EEPROM_PWR_SAVE_MODE 1
@@ -20,6 +22,8 @@
 #define MENU_RPM 2
 #define MENU_POWER 3
 #define MENU_VOLTAGE 4
+#define MENU_TEMP 5
+#define MENU_LED 6
 
 Adafruit_SSD1306 display(128, 32, &Wire, -1);
 boolean display_turned;
@@ -31,6 +35,7 @@ char str_tmp[6];
 
 uint8_t display_menu;
 boolean sleep_mode;
+boolean led_turned;
 
 boolean btn_pressed;
 boolean btn_long_pressed;
@@ -39,6 +44,7 @@ unsigned long btn_timer; // ms
 float wheel_length; // km
 float distance; // km
 
+volatile boolean wheel_rotated;
 boolean wheel_pin_enabled;
 uint16_t wheel_rotation_counter;
 unsigned long wheel_rotation_last_time; // ms
@@ -53,6 +59,7 @@ uint8_t speed_arr_index;
 void setup() {
     attachInterrupt(digitalPinToInterrupt(WHEEL_PIN), detect_rotation, RISING);
     pinMode(BTN_PIN, INPUT_PULLUP);
+    pinMode(LED_PIN, OUTPUT);
     
     Serial.begin(9600);
     delay(2000);
@@ -151,18 +158,42 @@ void display_data() {
             display.setCursor(0, 0);
             display.print("voltage:");
 
-            float v = read_voltage();
+            dtostrf(read_voltage(), 4, 1, str_tmp);
+            snprintf(buf, BUF_SIZE, "%s v", str_tmp);
 
             display.setCursor(0, 16);
-            display.print(v + String(" v"));
-            
-            if (v < 3.0) { // charge
-                display.print(" !!!");
-            }
+            display.print(buf);
             break;
+
+        case MENU_TEMP:
+            display.setCursor(0, 0);
+            display.print("temp:");
+
+            snprintf(buf, BUF_SIZE, "%d v", read_temp());
+            display.print(buf);
+            break
+
+        case MENU_LED:
+            display.setCursor(0, 0);
+            display.print("led:");
+
+            display.setCursor(0, 16);
+            display.print(led_turned ? "on" : "off");
+            break
     }
 
     display.display();
+}
+
+uint8_t read_temp() {
+    int reading = analogRead(TEMP_PIN);
+
+    // преобразовываем полученные данные в напряжение. Если используем Arduino 3.3 В, то меняем константу на 3.3
+    float voltage = reading * 5.0;
+    voltage /= 1024.0;
+
+    //конвертируем 10 мВ на градус с учетом отступа 500 мВ
+    return (voltage - 0.5) * 100 ;
 }
 
 float read_voltage() {
@@ -186,6 +217,7 @@ void enable_pwr_save_mode(boolean enable) {
 
 void enable_sleep_mode() {
     turn_display(false);
+    turn_led(false);
     sleep_mode = true;
     LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
 }
@@ -213,6 +245,34 @@ void calc_avg_speed(float speed) {
     }
 }
 
+void turn_led(boolean on) {
+    led_turned = on;
+
+    TCCR1A = 0;
+    TCCR1B = 0;
+    TCNT1 = 0;
+
+    if (on) {
+        // disable interrupts
+        cli();
+
+        // set ctc mode
+        TCCR1B |= (1<<WGM12);
+
+        // set counter max value for 250 ms
+        OCR1A = 3906;
+        
+        // set 1024 prescaler
+        TCCR1B |= (1<<CS12) | (1<<CS10);
+
+        // enable timer compare interrupt
+        TIMSK1 |= (1<<OCIE1A);
+
+        // allow interrupts
+        sei();
+    }
+}
+
 void handle_btn_click(uint8_t pin_state, unsigned long timer_now) {
     // click start
     if (!btn_pressed && pin_state == LOW) {
@@ -233,36 +293,35 @@ void handle_btn_click(uint8_t pin_state, unsigned long timer_now) {
     // handle long button click
     if (btn_pressed && !btn_long_pressed && ((timer_now - btn_timer) >= LONG_PRESS_TIME)) {
         btn_long_pressed = true;
-        if (display_menu == MENU_POWER) {
-            enable_pwr_save_mode(!pwr_save_mode);
-            display_data();
-            if (pwr_save_mode) {
-                wheel_rotation_last_time = timer_now; // fixme
-            }
-        } else {
-            turn_display(!display_turned); // todo turn on bluetooth
+        switch (display_menu) {
+            case MENU_POWER:
+                enable_pwr_save_mode(!pwr_save_mode);
+                display_data();
+                if (pwr_save_mode) {
+                    wheel_rotation_last_time = timer_now; // fixme
+                }
+                break;
+            case MENU_LED:
+                turn_led(!led_turned);
+                display_data();
+                break;
+            default:
+                turn_display(!display_turned); // todo turn on bluetooth
+                break;
         }
     }
 }
 
 void detect_rotation() {
-    unsigned long timer_now = millis();
-    wheel_rotation_last_time = timer_now;
-    if (wheel_rotation_start_time == 0) {
-        wheel_rotation_start_time = timer_now;
-    }
-
-    wheel_rotation_counter++;
-    distance += wheel_length;
+    wheel_rotated = true;
 }
 
 void calc_speed(unsigned long timer_now) {
     if (wheel_rotation_counter == WHEEL_ROTATION_MAX) {
         unsigned long interval = timer_now - wheel_rotation_start_time;
         float avg_interval = interval / WHEEL_ROTATION_MAX;
-        avg_interval = 1000 / avg_interval;
 
-        uint16_t rpm = avg_interval * 60;
+        uint16_t rpm = 60000 / avg_interval;
         if (rpm < WHEEL_RPM_MAX) {
             wheel_rpm = rpm;
             speed = wheel_rpm * 60 * wheel_length;
@@ -288,6 +347,18 @@ void loop() {
     unsigned long timer_now = millis();
 
     handle_btn_click(digitalRead(BTN_PIN), timer_now);
+
+    if (wheel_rotated) {
+        wheel_rotated = false;
+        wheel_rotation_last_time = timer_now;
+        if (wheel_rotation_start_time == 0) {
+            wheel_rotation_start_time = timer_now;
+        }
+
+        wheel_rotation_counter++;
+        distance += wheel_length;
+    }
+
     calc_speed(timer_now);
     
     // idle
@@ -302,7 +373,7 @@ void loop() {
     }
     
     // sleep
-    if (timer_now - wheel_rotation_last_time >= 20000) { // 20 sec
+    if (timer_now - wheel_rotation_last_time >= 20000) {
         wheel_rotation_last_time = 0;
         if (pwr_save_mode) {
             enable_sleep_mode();
@@ -323,5 +394,5 @@ void loop() {
         }
     }
 
-    delay(10);
+    delay(2);
 }
