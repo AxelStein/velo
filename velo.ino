da61076e0afdefb7d6bcd69cd027a3e44b49be77
DieFaust <diefaust1993@gmail.com>
2020-07-20 22:04:39 +0300

    Add gyver btn
    Todo: fix timers

--- velo.ino
+++ velo.ino
@@ -1,14 +1,14 @@
 #include <EEPROM.h>
-#include "std_format.h"
-#include "speedometer.h"
 #include <SSD1306Ascii.h>
 #include <SSD1306AsciiAvrI2c.h>
 
+#include "GyverButton.h"
+#include "std_format.h"
+#include "speedometer.h"
+
 #define WHEEL_PIN 2
 #define BTN_PIN 3
-#define LED_PIN 4
-#define VIBRO_PIN 5
-#define SINGLE_PRESS_TIME 50
+#define LED_PIN 5
 #define LONG_PRESS_TIME 500
 #define MENU_MAIN 0
 #define MENU_SPEED 1
@@ -25,28 +25,22 @@ Speedometer sp;
 SSD1306AsciiAvrI2c display;
 bool display_turned;
 uint8_t display_menu;
-uint32_t display_timer;
+// uint32_t display_timer;
 char buf[11];
+volatile bool upd_time;
+volatile bool upd_speed;
 
 bool led_turned;
 uint32_t led_timer;
 
-bool btn_pressed;
-bool btn_long_pressed;
-uint32_t btn_timer;
-
-/*
-uint32_t drink_water_timer;
-bool drink_water_reminder_enabled;
-uint32_t vibro_timer;
-uint8_t vibro_counter;
-*/
+GButton btn(BTN_PIN);
 
 void setup() {
     pinMode(BTN_PIN, INPUT_PULLUP);
     pinMode(LED_PIN, OUTPUT);
-    pinMode(VIBRO_PIN, OUTPUT);
-
+    
+    init_timers();
+    
     sp.init(WHEEL_PIN, calc_wheel_length(), EEPROM_TOTAL_DISTANCE);
     init_display();
 }
@@ -59,59 +53,96 @@ void init_display() {
     display_data();
 }
 
+void init_timers() {
+    cli();
+    
+    TCCR1A = 0;
+    TCCR1B = 0;
+    TCNT1 = 0;
+
+    // set ctc mode
+    TCCR1A |= _BV(WGM12);
+    TCCR1B |= _BV(WGM12);
+
+    OCR1A = 15625;  // update time every sec
+    OCR1B = 62500;  // update speed every 4 sec
+    
+    // set 1024 prescaler
+    TCCR1A |= _BV(CS12) | _BV(CS10);
+    TCCR1B |= _BV(CS12) | _BV(CS10);
+
+    // enable timer compare interrupt
+    TIMSK1 |= _BV(OCIE1A) | _BV(OCIE1B);
+    TIFR1 |= _BV(OCF1A) | _BV(OCF1B);
+
+    sei();
+}
+
+ISR(TIMER1_COMPA_vect) {
+    if (display_menu == MENU_TIME) {
+        upd_time = true;
+    }
+}
+
+ISR(TIMER1_COMPB_vect) {
+    if (display_menu == MENU_MAIN || display_menu == MENU_RPM) {
+        upd_speed = true;
+    }
+}
+
 void loop() {
     sp.detect_rotation();
     if (sp.idle()) {
         display_data();
     }
 
-    handle_btn_click();
-    
-    uint32_t timer_now = millis();
-    uint32_t timer_diff = timer_now - display_timer;
-
-    // update timer every sec
-    bool upd_time = display_menu == MENU_TIME && timer_diff >= 1000;
-
-    // update speed every 4 sec
-    bool upd_speed = (display_menu == MENU_MAIN || display_menu == MENU_RPM) && sp.get_speed() > 0 && timer_diff >= 4000;
-    
-    if (upd_time || upd_speed) {
+    btn.tick();
+    if (btn.isClick()) {
+        switch_display_menu();
         display_data();
-        display_timer = timer_now;
+    }
+    if (btn.isHolded()) {
+        switch (display_menu) {
+            case MENU_DISTANCE:
+                sp.save_total_distance();
+                display_data();
+                break;
+                
+            case MENU_LED:
+                turn_led(!led_turned);
+                display_data();
+                break;
+                
+            default:
+                turn_display(!display_turned);
+                break;
+        }
+    }
+    
+    if (upd_time || (upd_speed && sp.get_speed() > 0)) {
+        display_data();
+        upd_time = false;
+        upd_speed = false;
+        // display_timer = timer_now;
     }
 
     // toggle led every 250 ms
+    uint32_t timer_now = millis();
     if (led_turned && (timer_now - led_timer) >= 250) {
         PORTD ^= _BV(LED_PIN);
         led_timer = timer_now;
     }
-
-    /*
-    // enable drink water reminder every 20 min
-    // todo timer
-    timer_diff = timer_now - drink_water_timer;
-    if (timer_diff >= 1383000) {
-        drink_water_reminder_enabled = true;
-        drink_water_timer = timer_now;
-    }
-
-    // turn on vibro
-    if (drink_water_reminder_enabled) {
-        timer_diff = timer_now - vibro_timer;
-        if (timer_diff >= 250) {
-            PORTD ^= _BV(VIBRO_PIN);
-            vibro_timer = timer_now;
-            vibro_counter++;
-        }
-        // turn off vibro after 2 sec
-        if (vibro_counter == 9) {
-            drink_water_reminder_enabled = false;
-            vibro_counter = 0;
-        }
-    }
-    */
 }
+
+/*
+    uint32_t timer_diff = timer_now - display_timer;
+    
+    // update time every sec
+    bool upd_time = display_menu == MENU_TIME && timer_diff >= 1000;
+
+    // update speed every 4 sec
+    bool upd_speed = (display_menu == MENU_MAIN || display_menu == MENU_RPM) && sp.get_speed() > 0 && timer_diff >= 4000;
+    */
 
 float calc_wheel_length() {
     uint8_t diameter = EEPROM.read(EEPROM_WHEEL_DIAMETER);  // cm
@@ -128,45 +159,6 @@ void set_wheel_diameter(uint8_t diameter) {
     }
 }
 
-void handle_btn_click() {
-    uint8_t pin_state = digitalRead(BTN_PIN);
-    uint32_t timer_now = millis();
-    
-    // click start
-    if (!btn_pressed && pin_state == LOW) {
-        btn_pressed = true;
-        btn_timer = timer_now;
-    }
-    
-    // handle single button click
-    if (btn_pressed && pin_state == HIGH) {
-        if (!btn_long_pressed && display_turned && ((timer_now - btn_timer) >= SINGLE_PRESS_TIME)) {
-            btn_pressed = false;
-            switch_display_menu();
-            display_data();
-        }
-        btn_long_pressed = false;
-    }
-    
-    // handle long button click
-    if (btn_pressed && !btn_long_pressed && ((timer_now - btn_timer) >= LONG_PRESS_TIME)) {
-        btn_long_pressed = true;
-        switch (display_menu) {
-            case MENU_DISTANCE:
-                sp.save_total_distance();
-                display_data();
-                break;
-            case MENU_LED:
-                turn_led(!led_turned);
-                display_data();
-                break;
-            default:
-                turn_display(!display_turned);
-                break;
-        }
-    }
-}
-
 void turn_led(boolean on) {
     led_turned = on;
     digitalWrite(LED_PIN, LOW);
@@ -178,15 +170,11 @@ void turn_display(boolean on) {
 }
 
 void switch_display_menu() {
-    display_menu++;
-    if (display_menu == 6) {
-        display_menu = 0;
-    }
+    if (++display_menu == 6) display_menu = 0;
+    display.clear();
 }
 
 void display_data() {
-    display.clear();
-    
     switch(display_menu) {
         case MENU_MAIN:
             // speed
@@ -196,7 +184,7 @@ void display_data() {
             display.print(" km/h ");
 
             // distance
-            display.setCursor(0, 16);
+            display.setCursor(0, 2);
             std_ftoa(sp.get_distance(), 3, buf);
             display.print(buf);
             display.print(" km ");
@@ -210,7 +198,7 @@ void display_data() {
             display.print(" km/h ");
             
             // avg speed
-            display.setCursor(0, 16);
+            display.setCursor(0, 2);
             std_ftoa(sp.get_avg_speed(), 1, buf);
             display.print(buf);
             display.print(" km/h ");
@@ -221,7 +209,7 @@ void display_data() {
             display.setCursor(0, 0);
             display.print("rpm: ");
 
-            display.setCursor(0, 16);
+            display.setCursor(0, 2);
             std_itoa(sp.get_wheel_rpm(), 0, buf);
             display.print(buf);
             display.print("  ");
@@ -231,7 +219,7 @@ void display_data() {
             display.setCursor(0, 0);
             display.print("total: ");
 
-            display.setCursor(0, 16);
+            display.setCursor(0, 2);
             std_ftoa(sp.get_total_distance(), 3, buf);
             display.print(buf);
             display.print(" km");
@@ -243,7 +231,7 @@ void display_data() {
             int hours = (sec / 3600ul);
             int minutes = (sec % 3600ul) / 60ul;
             int seconds = (sec % 3600ul) % 60ul;
-            
+
             display.setCursor(0, 0);
             display.print("time:");
 
@@ -265,8 +253,8 @@ void display_data() {
             display.setCursor(0, 0);
             display.print("led:");
 
-            display.setCursor(0, 16);
-            display.print(led_turned ? "on" : "off");
+            display.setCursor(0, 2);
+            display.print(led_turned ? "on " : "off");
             break;
     }
 }
